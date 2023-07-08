a1d9b64d8884e4599a1e1d2fde590590da094b6c
Manish_7 <manish@unitx.tech>
2023-07-08 00:01:43 -0400

    First Commit

--- SlidingWindow/count-the-subarrays-having-product-less-than-k.cpp
+++ SlidingWindow/count-the-subarrays-having-product-less-than-k.cpp
@@ -1,8 +1,5 @@
 /*
-
 Given an array of positive numbers, the task is to find the number of possible contiguous subarrays having product less than a given number k.
-
-
 */
 #include <bits/stdc++.h>
 using namespace std;
